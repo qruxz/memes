ade47e3f3cd1018c165e04378f345a8fa299f12f
Kamal <132643735+qruxz@users.noreply.github.com>
2024-08-07 19:39:05 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+let memeImg = document.querySelector(".meme-display img");
+let memeBtn = document.getElementById("generateBtn");
+
+let memeInfo = document.querySelector(".meme-info p");
+
+function updateMeme(url, title, author) {
+  memeImg.setAttribute("src", url);
+  memeInfo.innerHTML = `Title: ${title} <br> Author: ${author}`;
+}
+function getMeme() {
+  fetch("https://meme-api.com/gimme/nsfw-memes")
+    .then((res) => res.json())
+    .then((data) => {
+      updateMeme(data.url, data.title, data.author);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+getMeme();
+memeBtn.addEventListener("click", getMeme);
